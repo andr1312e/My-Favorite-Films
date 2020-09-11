5ba98372fe7877cad17aff4eeedb631c81843979
Andre Pilikin <andr1312e@outlook.com>
2020-09-11 11:53:06 +0300

    Add project files.

---
+++ GoodBooks.Services/IBookService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace GoodBooks.Services
+{
+    public interface IBookService
+    {
+        public void AddBook(Book Book);
+        public void DeleteBook(int bookId);
+        public List<Book> GetAllBooks();
+        public Book GetBook(int bookId);
+    }
+}
---
+++ GoodBooks.Web/Controllers/BooksController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GoodBooks.Web.Controllers
+{
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly ILogger<BooksController> _logger;
+
+        // The Web API will only accept tokens 1) for users, and 2) having the access_as_user scope for this API
+        static readonly string[] scopeRequiredByApi = new string[] { "access_as_user" };
+
+        public BooksController(ILogger<BooksController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/api/books")]
+        public ActionResult Get()
+        {
+            return Ok("books");
+        }
+    }
+}
